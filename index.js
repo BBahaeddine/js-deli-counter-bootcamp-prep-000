5088d331e6793817d6ec2137d5325f79c5465159
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-02-27 13:47:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
   } else {
     var message = "The line is currently:";
     for(var i = 0; i < katzDeliLine.length; i++) {
-      message += i+". "+ element+",";
+      message += ", " + i+". "+ katzDeliLine[i];
     }
     // console.log(message);
     
