faca89d7415a279cc535516d3c88ca8c88586ced
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-02-27 13:27:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ function takeANumber(katzDeliLine, name) {
   
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
+    var personName = katzDeliLine.shit();
+    return `Currently serving ${personName}`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
     
   }
+  
 }
